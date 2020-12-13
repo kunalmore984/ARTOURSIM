3af5edda9026759773466a4e0c2c0954b507d610
kunalmore984 <kunalmore2288@gmail.com>
2020-12-13 13:13:20 +0530

    BottonNavigation java file

--- app/src/main/java/com/example/ar_tour/BottomNavigation.java
+++ app/src/main/java/com/example/ar_tour/BottomNavigation.java
@@ -1,14 +1,66 @@
 package com.example.ar_tour;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class BottomNavigation extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.unity3d.player.UnityPlayerActivity;
+
+public class BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
+
+        //loading the default fragment
+        loadFragment(new HomeFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.explore:
+                /*Intent UnityActivity =new Intent(BottomNavigation.this, UnityPlayerActivity.class);
+                startActivity(UnityActivity);*/
+                fragment = new explore();
+                break;
+
+            case R.id.settings:
+                fragment = new settings();
+                break;
+
+            case R.id.trips:
+                fragment = new Trip();
+                break;
+        }
+
+        return loadFragment(fragment);
     }
 }
