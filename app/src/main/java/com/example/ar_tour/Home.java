d2bd8e6d0ba270c8c68468bd74d22d8726a58ad4
kunalmore984 <kunalmore2288@gmail.com>
2020-12-13 13:36:28 +0530

    home trail activity

--- app/src/main/java/com/example/ar_tour/Home.java
+++ app/src/main/java/com/example/ar_tour/Home.java
@@ -1,14 +1,118 @@
 package com.example.ar_tour;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.unity3d.player.UnityPlayerActivity;
+
+import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ArrayList<String> Number;
+    RecyclerView.LayoutManager RecyclerViewLayoutManager;
+    CustomAdapter RecyclerViewHorizontalAdapter;
+    LinearLayoutManager HorizontalLayout ;
+    View ChildView ;
+    int RecyclerViewItemPosition ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //button to start explore page.......
+/*        Button nav = (Button) findViewById(R.id.explore);
+        nav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent NavigationIntent = new Intent(Home.this, MainActivity.class);
+                startActivity(NavigationIntent);
+            }
+        });*/
+        recyclerView = (RecyclerView)findViewById(R.id.Recylerview);
+
+        RecyclerViewLayoutManager = new LinearLayoutManager(getApplicationContext());
+
+        recyclerView.setLayoutManager(RecyclerViewLayoutManager);
+
+        // Adding items to RecyclerView.
+        AddItemsToRecyclerViewArrayList();
+
+        RecyclerViewHorizontalAdapter = new CustomAdapter(Number);
+
+        HorizontalLayout = new LinearLayoutManager(Home.this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(HorizontalLayout);
+
+        recyclerView.setAdapter(RecyclerViewHorizontalAdapter);
+
+
+        // Adding on item click listener to RecyclerView.
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+
+            GestureDetector gestureDetector = new GestureDetector(Home.this, new GestureDetector.SimpleOnGestureListener() {
+
+                @Override public boolean onSingleTapUp(MotionEvent motionEvent) {
+
+                    return true;
+                }
+
+            });
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView Recyclerview, MotionEvent motionEvent) {
+
+                ChildView = Recyclerview.findChildViewUnder(motionEvent.getX(), motionEvent.getY());
+
+                if(ChildView != null && gestureDetector.onTouchEvent(motionEvent)) {
+
+                    //Getting clicked value.
+                    RecyclerViewItemPosition = Recyclerview.getChildAdapterPosition(ChildView);
+
+                    // Showing clicked item value on screen using toast message.
+                    Toast.makeText(Home.this, Number.get(RecyclerViewItemPosition), Toast.LENGTH_LONG).show();
+
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView Recyclerview, MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+
+    }
+
+    // function to add items in RecyclerView.
+    public void AddItemsToRecyclerViewArrayList(){
+
+        Number = new ArrayList<>();
+        Number.add("Trip Offer 1");
+        Number.add("Trip Offer 2");
+        Number.add("Trip Offer 3");
+        Number.add("Trip Offer 4");
+        Number.add("Trip Offer 5");
+        Number.add("Trip Offer 6");
+        Number.add("Trip Offer 7");
+        Number.add("Trip Offer 8");
+        Number.add("Trip Offer 9");
+        Number.add("Trip Offer 10");
+
     }
 }
