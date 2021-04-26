dcea6363ca622c6d58320fd1ed1fc56d9677a33f
kunalmore984 <kunalmore2288@gmail.com>
2021-04-26 21:17:07 +0530

    modified base activities

--- app/src/main/java/com/example/ar_tour/Bookings.java
+++ app/src/main/java/com/example/ar_tour/Bookings.java
@@ -1,44 +1,29 @@
 package com.example.ar_tour;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-import com.squareup.picasso.Picasso;
+import com.google.android.material.tabs.TabLayout;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link settings#newInstance} factory method to
+ * Use the {@link Bookings#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class settings extends Fragment {
+public class Bookings extends Fragment {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    TabAdapter tabAdapter;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,7 +38,7 @@ public class settings extends Fragment {
 
 
 
-    public settings() {
+    public Bookings() {
         // Required empty public constructor
     }
 
@@ -66,8 +51,8 @@ public class settings extends Fragment {
      * @return A new instance of fragment settings.
      */
     // TODO: Rename and change types and number of parameters
-    public static settings newInstance(String param1, String param2) {
-        settings fragment = new settings();
+    public static Bookings newInstance(String param1, String param2) {
+        Bookings fragment = new Bookings();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -87,7 +72,23 @@ public class settings extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        tabLayout =view.findViewById(R.id.explore_tab);
+        viewPager =view.findViewById(R.id.explore_viewpager);
+        tabAdapter =new TabAdapter(getChildFragmentManager(),FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        tabAdapter.addfragments(new FlightFragment(),"Flights");
+        //tabAdapter.addfragments(new Trains(),"Trains");
+        tabAdapter.addfragments(new Hotels(),"Hotels");
+        viewPager.setAdapter(tabAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bookings, container, false);
     }
     
 }
